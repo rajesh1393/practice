016a4e9184deb1bdbecc19e0e359a6863cd486af
rajesh1393 <rajeshangler@gmail.com>
2020-03-22 17:45:08 +0530

    Added 2 testcases and modified one test case

--- src/Testcases/NewTest_2.java
+++ src/Testcases/NewTest_2.java
@@ -11,6 +11,6 @@ public class NewTest {
 	  System.setProperty("webdriver.chrome.driver", "F:\\Git_code\\practice\\Parallel_testing\\Updated_Jar\\Driver\\chromedriver.exe");
 		 
 	  driver = new ChromeDriver();
-	  driver.get("https://www.google.co.in"); 
+	  driver.get("http://www.store.demoqa.com"); 
   }
 }
