8c472cacaa819d0dbff9feccf4a89fbd21e2fd4d
rajesh1393 <rajeshangler@gmail.com>
2020-03-22 18:12:21 +0530

    meging rajesh with masters

---
+++ src/Testcases/NewTest_4.java
@@ -0,0 +1,16 @@
+package Testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	protected static WebDriver driver;
+  @Test
+  public void f() {
+	  System.setProperty("webdriver.chrome.driver", "F:\\Git_code\\practice\\Parallel_testing\\Updated_Jar\\Driver\\chromedriver.exe");
+		 
+	  driver = new ChromeDriver();
+	  driver.get("https://www.google.com"); 
+  }
+}
